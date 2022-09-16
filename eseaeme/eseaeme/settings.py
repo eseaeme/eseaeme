a9ee23eec59434931a66e80e05890e2ac4d53bf0
saya4eat <71570463+saya4eat@users.noreply.github.com>
2022-09-16 18:21:38 +0200

    adding db

--- eseaeme/eseaeme/settings.py
+++ eseaeme/eseaeme/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'eseaeme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST':'localhost',
+        'PORT':'3306',
+        'USER':'root',
+        'PASSWORD':'123456',
+        'NAME':'eseaeme_db',
     }
 }
 
